b6a564e7ce25b712656bf1ca988af32589571743
shadabshaikh786 <162874816+shadabshaikh247302@users.noreply.github.com>
2025-01-22 01:03:39 +0530

    Update monthlySchema.js

--- schema/monthlySchema.js
+++ schema/monthlySchema.js
@@ -1,48 +1,23 @@
-<<<<<<< HEAD
 const { default: mongoose } = require("mongoose");
 
 const monthlySchema = mongoose.Schema({
-    tittle:{
-        type:String,
-        required:true
+    tittle: {
+        type: String,
+        required: true
     },
-    creator:{
-        type:String
+    creator: {
+        type: String
     },
-    day:{
-        type:String
+    day: {
+        type: String
     },
-    month:{
-        type:String
+    month: {
+        type: String
     },
-    year :{
-        type : String
+    year: {
+        type: String
     }
-})
+});
 
-const Mschema = mongoose.model("Mschema",monthlySchema)
-=======
-const { default: mongoose } = require("mongoose");
-
-const monthlySchema = mongoose.Schema({
-    tittle:{
-        type:String,
-        required:true
-    },
-    creator:{
-        type:String
-    },
-    day:{
-        type:String
-    },
-    month:{
-        type:String
-    },
-    year :{
-        type : String
-    }
-})
-
-const Mschema = mongoose.model("Mschema",monthlySchema)
->>>>>>> 35700e10c5bcf2b3cadd5613a041d48be0bf2b5f
-module.exports = Mschema
+const Mschema = mongoose.model("Mschema", monthlySchema);
+module.exports = Mschema;
