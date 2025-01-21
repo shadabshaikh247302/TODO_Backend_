40c211783b4c9a9a38ca2e7f6c12c8385cf5a420
shadabshaikh786 <162874816+shadabshaikh247302@users.noreply.github.com>
2025-01-22 01:05:04 +0530

    Update user.js

--- schema/user.js
+++ schema/user.js
@@ -1,58 +1,27 @@
-<<<<<<< HEAD
 const { default: mongoose } = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    username:{
-        type:String,
-        required:true
+    username: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,   
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    confirm_password:{
-        type:String,
-        required:true
+    confirm_password: {
+        type: String,
+        required: true
     },
-    profilePicture:{
-        type:String
+    profilePicture: {
+        type: String
     }
+});
 
-})
-
-const Uschema = mongoose.model("Uschema",userSchema)
-=======
-const { default: mongoose } = require("mongoose");
-
-const userSchema = mongoose.Schema({
-    username:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,   
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    confirm_password:{
-        type:String,
-        required:true
-    },
-    profilePicture:{
-        type:String
-    }
-
-})
-
-const Uschema = mongoose.model("Uschema",userSchema)
->>>>>>> 35700e10c5bcf2b3cadd5613a041d48be0bf2b5f
-module.exports = Uschema
+const Uschema = mongoose.model("Uschema", userSchema);
+module.exports = Uschema;
